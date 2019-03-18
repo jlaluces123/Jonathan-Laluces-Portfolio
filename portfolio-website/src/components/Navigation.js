f8567bea0ba9154d47f3291b1c0f8e4cb96d0da4
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-18 10:15:20 -0700

    Base Functionality: Styled Navigation accordingly, now working on function

--- portfolio-website/src/components/Navigation.js
+++ portfolio-website/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/Navigation.css";
 
 export const Navigation = (props) => {  
 
@@ -6,9 +7,12 @@ export const Navigation = (props) => {
     <div className = "navigation-wrapper">
       <h3 className = "navigation-header">Jonathan Laluces</h3>
 
-      <div className = "hamburger-menu" onClick = {props.clickMenu}>
-        <button className = "close-toggle">X</button>
+      <div className = "hamburger-menu">
+        <svg style={{ width: 40, height: 40 }} viewBox="0 0 24 24" onClick = {props.clickMenu}>
+          <path fill="#fff" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+        </svg>        
         <ul className = "pages-list">
+          <button className = "close-toggle">X</button>
           <li><a href="/">Home</a></li>
           <li><a href="/">About</a></li>
           <li><a href="/">Projects</a></li>
