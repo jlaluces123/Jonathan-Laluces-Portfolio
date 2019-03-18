f8142b1e833af8ea8649fc059385a64234e91c81
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-18 11:27:31 -0700

    Bare Bones Done: Rough implementation of navigation bar done

--- portfolio-website/src/components/Navigation.js
+++ portfolio-website/src/components/Navigation.js
@@ -26,14 +26,20 @@ class Navigation extends React.Component {
               <path fill="#fff" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
             </svg>  
           </a>      
-          <ul className = {this.state.active ? "active-style" : "pages-list"}>
-            <a className = "toggle" onClick = {this.clickMenu}>X</a>
-            <li><a href="/">Home</a></li>
-            <li><a href="/">About</a></li>
-            <li><a href="/">Projects</a></li>
-            <li><a href="/">Blog</a></li>
-            <li><a href="/">Contact</a></li>
-          </ul>
+          <div className = {this.state.active ? "active-style" : "pages-list"}>
+            <a className = "toggle" onClick = {this.clickMenu}>
+              <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
+                <path fill="#fff" d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z" />
+              </svg>
+            </a>
+            <ul className = "pages-menu">              
+              <li><a className = "pages-menu-item" href="#home">Home</a></li>
+              <li><a className = "pages-menu-item" href="#about">About</a></li>
+              <li><a className = "pages-menu-item" href="#projects">Projects</a></li>
+              <li><a className = "pages-menu-item" href="#blog">Blog</a></li>
+              <li><a className = "pages-menu-item" href="#contact">Contact</a></li>
+            </ul>
+          </div>
         </div>
   
       </div>
