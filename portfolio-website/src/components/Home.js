bd6c49e6d10a2289f45270bbc8469c03e66ca57b
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-19 15:03:38 -0700

    Typing Element: Fixed typing element for banner

--- portfolio-website/src/components/Home.js
+++ portfolio-website/src/components/Home.js
@@ -9,11 +9,16 @@ import Typing from "react-typing-animation";
 export const Home = () => {
   return (
     <div className = "home-wrapper" id = "home">
+
+    <div className = "typing-overlay">
       <Typing className = "typing-component">
-        <h1 className = "intro-text">console.log("Hello Wordl");</h1>                
-        <Typing.Backspace count = {5} />        
-        <h1 className = "intro-text">ld");</h1>
-      </Typing>      
+        <h1 className = "intro-text">return "Hello Wordl";</h1>                
+        <Typing.Backspace count = {4} />        
+        <h1 className = "intro-text">ld";</h1>
+      </Typing>  
+      </div> 
+
+
     </div>
   );
 };
