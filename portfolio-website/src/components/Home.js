a0f51492ba4575bfd22203414c2a7566a7b73ded
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-19 14:40:49 -0700

    Typing Animation: Animation implemented, styling later

--- portfolio-website/src/components/Home.js
+++ portfolio-website/src/components/Home.js
@@ -1,10 +1,19 @@
 import React from "react";
+
+// --> Style sheet
 import "../styles/Home.css";
+
+// --> Module Imports
+import Typing from "react-typing-animation";
 
 export const Home = () => {
   return (
     <div className = "home-wrapper" id = "home">
-      <h1 className = "home-name">I am the home component</h1>
+      <Typing className = "typing-component">
+        <h1 className = "intro-text">console.log("Hello Wordl");</h1>                
+        <Typing.Backspace count = {5} />        
+        <h1 className = "intro-text">ld");</h1>
+      </Typing>      
     </div>
   );
 };
