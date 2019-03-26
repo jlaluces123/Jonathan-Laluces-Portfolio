ccd8a6141dd738bfe95f283fb08a0175cc350901
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-26 10:18:56 -0700

    Scrapping Idea: Going to use formspree instead to send emails directly to my address

--- portfolio-website/src/components/Contact.js
+++ portfolio-website/src/components/Contact.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactContactForm from 'react-mail-form';
 
 export const Contact = props => {
   return (
-    <h1>Contact</h1>
+    <div className = "contact-form-wrapper">      
+    </div>
   );
 };
