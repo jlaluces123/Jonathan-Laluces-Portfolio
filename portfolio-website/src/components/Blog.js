9d76b10dda8dff4cb74889327d378f937a63c6ac
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-26 09:50:45 -0700

    Final Update: Blog section done, will add more once I write the next batch of blog posts

--- portfolio-website/src/components/Blog.js
+++ portfolio-website/src/components/Blog.js
@@ -17,18 +17,21 @@ export const Blog = props => {
             </a>
           </div>
 
-          <p className = "date">Mar 22, 2019</p>
+          <p className = "date white">Mar 22, 2019</p>
           <h3 className = "blog-title">
-            <a className = "title-link" href = "https://medium.com/@jlalucescareer123/3-things-i-wish-i-could-tell-myself-to-prep-for-coding-bootcamp-c6d4dc6aa787" target = "_blank">
+            <a className = "title-link white" href = "https://medium.com/@jlalucescareer123/3-things-i-wish-i-could-tell-myself-to-prep-for-coding-bootcamp-c6d4dc6aa787" target = "_blank">
               3 Things I Wish I Could Tell Myself To Prep for Coding Bootcamp
             </a>
           </h3>
-          <p className = "blog-description">
+          <p className = "blog-description white">
             Before we begin, let me introduce myself! My name is Jonathan, no “h” before the “n”, 
             and yes I do go by “Jon” if you like. I joined Lambda School a month after 
             graduating high school in 2018, and have been a student at the Lambda School 
             Web Development Program for over 6 months.
           </p>
+          <a href = "https://medium.com/@jlalucescareer123/3-things-i-wish-i-could-tell-myself-to-prep-for-coding-bootcamp-c6d4dc6aa787" target = "_blank" className = "read-more white">
+            READ MORE &#x2192;
+          </a>
         </article>
       </div>
     </div>
