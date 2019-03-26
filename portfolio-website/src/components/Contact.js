504576df12fb0956309dec8a6ed532ef8a0e6c95
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-26 10:24:22 -0700

    Helper Function: Added a handle submit function and added it onto the for m submit

--- portfolio-website/src/components/Contact.js
+++ portfolio-website/src/components/Contact.js
@@ -6,12 +6,16 @@ class Contact extends React.Component {
     e.preventDefault(); // --> Prevent Bubbling up
     const { name, value } = e.target; // --> De-reference for clarity
     this.setState({ [name]: value });
-  }
+  };
+
+  handleSubmit = e => {
+    console.log("submitted successfully");
+  };
 
   render() {
     return (
       <div className = "contact-form-wrapper">  
-        <form action="https://formspree.io/jlalucescareer123@gmail.com" method="POST">
+        <form onSubmit = {this.handleSubmit} action="https://formspree.io/jlalucescareer123@gmail.com" method="POST">
   
         </form>
       </div>
