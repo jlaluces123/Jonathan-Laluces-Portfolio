b1039f028a987f66f929f7506b29f5c5aef2853a
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-25 17:22:57 -0700

    Component set up: starting section layout

--- portfolio-website/src/components/Blog.js
+++ portfolio-website/src/components/Blog.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "../styles/Blog.css";
+
+import Backend from "../images/back-end.jpg";
 
 export const Blog = props => {
   return (
-    <h1>Writing</h1>
+    <div className = "blog-wrapper">
+
+      <h1 className = "blog-wrapper-header">Writing</h1>
+
+      <article className = "blog-box-wrapper">
+
+        <div className = "blog-image">          
+          <a href = "https://medium.com/@jlalucescareer123/3-things-i-wish-i-could-tell-myself-to-prep-for-coding-bootcamp-c6d4dc6aa787" target = "_blank">
+            <img className = "blog-cover" src = {Backend} alt = "back-end code" />
+          </a>
+        </div>
+
+
+      </article>
+    </div>
   );
 };
