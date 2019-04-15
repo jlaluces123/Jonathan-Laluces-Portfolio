de455c98e1e7879847ccfe78c925cc8be353edd9
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-04-15 11:41:53 -0700

    New Project: Added a code challenege project onto the projects section

--- portfolio-website/src/components/Projects.js
+++ portfolio-website/src/components/Projects.js
@@ -4,6 +4,7 @@ import "../styles/Projects.css";
 import ScreenShotOne from "../images/Events Page ScreenShot.png";
 import ScreenShotTwo from "../images/TakeNotes.png";
 import ScreenShotThree from "../images/DevBlog.png";
+import ScreenShotFour from "../images/Huddle.png";
 
 export const Projects = props => {
   return (
@@ -93,6 +94,31 @@ export const Projects = props => {
           </p>
 
           <a className = "learn-more" rel="noopener noreferrer" href = "https://hungry-shannon-d42678.netlify.com/" target = "_blank">Learn More</a>
+        </div>
+      </div>
+
+      <div className = "row row-2">
+         {/* Picture of App */}
+         <div className = "app-pic-wrapper">
+          <a rel="noopener noreferrer" href = "https://unruffled-ride-b6c4c7.netlify.com" target = "_blank">
+            <img src = {ScreenShotFour} alt = "events-page" className = "app-screen-shot" />
+          </a>
+        </div>
+
+        {/* Text Description */}
+        <div className = "app-text-wrapper text-wrap">
+          <h1 className = "app-header">Huddle Landing Page Code Challenge</h1>
+
+          <p className = "app-description">
+            This is a front-end code challenge I did from Front End Mentor to freshen up on my base front-end
+            skills.
+          </p>
+
+          <p className = "app-description">
+            Front End Tech: HTML, CSS
+          </p>          
+
+          <a className = "learn-more" rel="noopener noreferrer" href = "https://unruffled-ride-b6c4c7.netlify.com" target="_blank">Learn More</a>
         </div>
       </div>
     </div>
