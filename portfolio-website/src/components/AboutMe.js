fe28d44e4b46d7bd0d290a372933a6bc9483679d
jlaluces123 <jlalucescareer123@gmail.com>
2019-05-20 17:43:52 -0700

    Change again

--- portfolio-website/src/components/AboutMe.js
+++ portfolio-website/src/components/AboutMe.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../styles/AboutMe.css";
 
 export const AboutMe = (props) => {
-
   return (
     <div className = "about-me-wrapper"> 
       <div className = "left-side">
